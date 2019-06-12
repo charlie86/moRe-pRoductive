7d3a43290683083f4b08904b8fc1f4adbcbedc31
Dan Ovando <DanOvando@users.noreply.github.com>
2019-06-12 15:03:49 -0700

    Fix path and add metadata to splitter.R
    thanks @charlie86, works great! Made a few small changes, fixed the file path to `disc_`, `og_filepath` name, and added title, track, and disc to the metadata (that way those show up when imported to things like itunes)

--- splitter.R
+++ splitter.R
@@ -7,17 +7,20 @@ plan(multiprocess)
 discs <- read.csv('metadata.csv', stringsAsFactors = F)
 
 future_map(1:nrow(discs), function(x) {
-  if (!dir.exists(paste0('discs_split/disc', discs$disc[x]))) {
-    dir.create(paste0('discs_split/disc', discs$disc[x]))
+  if (!dir.exists(paste0('discs_split/disc_', discs$disc[x]))) {
+    dir.create(paste0('discs_split/disc_', discs$disc[x]))
   }
 
   filepath <- str_glue('discs_original/Radiohead - MINIDISCS -HACKED- - {str_pad(discs$disc[x], width = 2, side = "left", pad = 0)} MD{discs$disc[x] + 110}.mp3')
   
-  command <- str_glue('ffmpeg -i "{og_filepath}" -ss {start_time} -t {length} "{outfile}.mp3"',
+command <- str_glue('ffmpeg -i "{og_filepath}" -metadata title="{title}" -metadata track={track} -metadata disc={disc} -ss {start_time} -t {length} "{outfile}.mp3"',
                       disc = discs$disc,
                       start_time = discs$start_time[x],
-                      og_filepath = og_filepath,
+                      og_filepath = filepath,
                       length = discs$length[x],
+                      title = discs$title[x],
+                      track = discs$track_number[x],
+                      disc = discs$disc[x],
                       outfile = paste0('discs_split/disc_', discs$disc[x], '/', discs$title[x]))
   system(command, intern = TRUE)
 }, .progress = TRUE)
